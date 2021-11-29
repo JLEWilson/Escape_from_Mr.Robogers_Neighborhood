d9d976c79bfc67b1ab25ac91ab5e48106fd47e5c
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-29 12:00:43 -0800

    make a functional player and canvas
    Co-authored-by: Christy Welch <christinamawel@gmail.com>

--- src/js/DisplayObjects.js
+++ src/js/DisplayObjects.js
@@ -1,71 +1,75 @@
-//canvas
-//context
-//objects to draw
-//loop
-
-class Animator{
+export class Animator{
   constructor(){
-    this.screen = window.getElementById('primaryDisplayModule');
+    this.screen = document.getElementById('primaryDisplayModule');
+    this.screen.height = window.innerHeight*0.7;
+    this.screen.width = this.screen.height;
     this.brush = this.screen.getContext('2d');
+    this.q = this.screen.height/10;
+  }
+  clear(){
+    this.brush.clearRect(0,0,this.screen.width,this.screen.height);
   }
 }
 
-class Player{
+export class Player{
   constructor(ani,levelSet){
-    this.x = 0;
-    this.y = 0;
+    this.x = 3;
+    this.y = 3;
     this.z = 0;
-    this.width = 1;
-    this.height = 1;
-    this.texture = 'red';
     this.animator = ani;
+    this.width = 1*this.animator.q;
+    this.height = 1*this.animator.q;
+    this.texture = 'red';
     this.levelSet = levelSet;
   }
   drawSelf(){
-    this.animator.brush.fillStyle(this.texture);
-    this.animator.brush.fillRect(this.x,this.y,this.width, this.height);
+    this.animator.brush.fillStyle = this.texture;
+    this.animator.brush.fillRect(this.x*this.animator.q,this.y*this.animator.q,this.width, this.height);
   }
   move(direction){
     if(direction === 'N'){
-      if(this.levelSet[this.z][this.x][this.y+1].tranparent){
-        this.y = this.y+1;
-      }
-    }
-    if(direction === 'S'){
-      if(this.levelSet[this.z][this.x][this.y-1].tranparent){
+      if(this.levelSet.Rooms[this.z][this.x][this.y-1].transparent){
         this.y = this.y-1;
       }
     }
+    if(direction === 'S'){
+      if(this.levelSet.Rooms[this.z][this.x][this.y+1].transparent){
+        this.y = this.y+1;
+      }
+    }
     if(direction === 'E'){
-      if(this.levelSet[this.z][this.x+1][this.y].tranparent){
+      if(this.levelSet.Rooms[this.z][this.x+1][this.y].transparent){
         this.x = this.x+1;
       }
     }
     if(direction === 'W'){
-      if(this.levelSet[this.z][this.x-1][this.y].tranparent){
+      if(this.levelSet.Rooms[this.z][this.x-1][this.y].transparent){
         this.x = this.x-1;
       }
     }
   }
 }
 
-class LevelMap{
+export class LevelMap{
   constructor(){
     this.Rooms = [
-      [new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
-      [new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black')]];
+      [
+        [new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(true,'white'),new Tile(false,'black')],
+        [new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black'),new Tile(false,'black')]
+      ]
+    ];
   }
 }
 
-class Tile{
+export class Tile{
   constructor(transparency, texture){
     this.transparent = transparency;
     this.texture = texture;
