1317fe2a6511166c7958d4e075edfc41cb51a9d4
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-29 14:44:08 -0900

    end day 1 team moondragon
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>
    Co-Authored-by: Zach Wilson <zwilson22495@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './css/styles.css';
+import $ from "jquery";
+import { Player, Enemy } from './js/entity';
+
+let player;
+const enemy = new Enemy("bug");
+
+$('#character-create-form').submit((event) => {
+  event.preventDefault();
+  const name = $('#charName').val();
+  player = new Player(name, 30, 30, 5, 3);
+  player.setBattleText(enemy);
+  enemy.setBattleText(player);
+  $('.story-area').append("<p>Greetings " + player.name + "!!!</p>");
+  $('.character-create').hide();
+  $('.battle-action').show();
+});
+
+$('#attack').click(() => {
+  const attack = player.attack(enemy);
+  if(attack > 0){
+    enemy.takeDamage(attack);
+    player.setBattleText(enemy);
+    if(enemy.isAlive){ 
+      $('.story-area').append("<p>" + player.attackText[player.randomAttackIndex] + "</p>");
+      console.log(player.attackText[0]);
+      console.log(player.randomAttackIndex);
+      const enemyAttack = enemy.attack(player);
+      if (enemyAttack > 0){
+        player.takeDamage(enemyAttack);
+        enemy.setBattleText(player);
+        if(player.isAlive) {
+          $('.story-area').append("<p>" + enemy.attackText[enemy.randomAttackIndex] + "</p>");
+        } else {
+          $('.story-area').append("<p>" + player.deathText[player.randomDeathIndex] + "</p>");
+        }
+      } else {
+        enemy.setBattleText(player);
+        $('.story-area').append("<p>" + enemy.missText[enemy.randomMissIndex] + "</p>");
+      }
+    } else {
+      $('.story-area').append("<p>Your attack put " + enemy.name + " below 0 cpu!</p>");
+      $('.story-area').append("<p>" + enemy.deathText[enemy.randomDeathIndex] + "</p>");
+    }
+  } else {
+    player.setBattleText(enemy);
+    $('.story-area').append("<p>" + player.missText[player.randomMissText] + "</p>");
+  }
+})
--- src/js/entity.js
+++ src/js/entity.js
@@ -1,29 +1,28 @@
-import { thisTypeAnnotation } from "@babel/types";
-
 export default class Entity{
   constructor(name, health, mana, attackDamage, armor){
-    name = this.name;
-    cpu = this.health;
-    ram = this.mana;
-    disk = this.attackDamage;
-    fanUsage = this.armor;
-    isAlive = true;
+    this.name = name;
+    this.cpu = health;
+    this.ram = mana;
+    this.disk = attackDamage;
+    this.fanUsage = armor;
+    this.isAlive = true;
   }
   attack(target){
-    const damage = this.attackDamage - target.fanUsage;
+    const damage = this.disk - target.fanUsage;
     if (damage <= 0) {
+      return 0;
     } else {
-      target.cpu -= damage;
-      if(target.cpu <=0 ){
-        target.die();
-      }
+      return damage;
+    }
+  }
+  takeDamage(damage){
+    this.cpu -= damage;
+    if(this.cpu <= 0 ){
+      this.die();
     }
   }
   guard(){
     //Halves damage taken for 1 turn
-  }
-  takeDamage(damage){
-    //random number in a range
   }
   die(){
     this.isAlive = false;
@@ -33,30 +32,33 @@ export default class Entity{
 export class Player extends Entity{
   constructor(name, health, mana, attackDamage, armor){
     super(name, health, mana, attackDamage, armor);
-    devices = {};
-    equippedDevice = {};
+    this.devices = {};
+    this.equippedDevice = {};
   }
   setBattleText(enemy){
     this.missText = [
     "Your aim is bad",
-    target.name + " is too cute to hurt",
-    "The " + target.name + " is hit for 0 damage!"
+    enemy.name + " is too cute to hurt",
+    "The " + enemy.name + " is hit for 0 damage!"
   ];
   this.attackText = [
-    "You insult the " + target.name +". " + target.name + " health reduced to " + target.cpu,
+    "You insult the " + enemy.name +". " + enemy.name + " health reduced to " + enemy.cpu,
   ];
   this.deathText = [
-    //what it says when you die
+    "You defeated the " + enemy.name + "!!!!", 
   ];
+  this.randomMissIndex = Math.floor( Math.random() * this.missText.length);
+  this.randomAttackIndex = Math.floor( Math.random() * this.attackText.length);
+  this.randomDeathIndex = Math.floor( Math.random() * this.deathText.length);
   }
   restore(stat){
-
+    console.log(stat);
   }
   debugCode(){
     //attack with mana
   }
   useItem(item){
-
+    console.log(item);
   }
   flee(){
 
@@ -67,30 +69,30 @@ export class Enemy extends Entity{
     switch (name){
       case "unknown error":
         super(name, 15, 0, 9, 4);
-        skills = [
+        this.skills = [
           {
             skillDamage: 10,
             skillText: "You bang your head on the closest hard object in frustration",
           }
-        ]
+        ];
         break;
       case "bug":
-        super(name, 5, 0, 3, 0 );
-        skills = [
+        super(name, 5, 0, 3, 1);
+        this.skills = [
           {
             skillDamage: 5,
             skillText: "Stops you from running correctly"
           }
-        ]
+        ];
         break;
       case "typo":
         super(name, 7, 3, 5, 2);
-        skills = [
+        this.skills = [
           {
             skillDamage: 8,
             skillText: "The typo moves too fast for you to see it"            
           }
-        ]
+        ];
         break;        
       default:
         super();
@@ -98,24 +100,21 @@ export class Enemy extends Entity{
   }
   setBattleText(player){
     this.missText = [
-     "Your aim is bad",
-     player.name + " is too cute to hurt",
-     "The " + player.name + " is hit for 0 damage!"
-   ];
-   this.attackText = [
-     "You insult the " + player.name +". " + player.name + " health reduced to " + player.cpu,
-   ];
-   this.deathText = [
-     "You fixed the bug!",
-     "This bug was an easy fix",
-   ];
-   }
-   wait(){
+      "The " + this.name + "'s aim is bad",
+      "The " + this.name + " thought you could use a break!"
+    ];
+    this.attackText = [
+      "The " + this.name + " annoyed " + player.name +". " + player.name + " health reduced to " + player.cpu,
+    ];
+    this.deathText = [
+      "You fixed the bug!",
+      "This bug was an easy fix",
+    ];
+    this.randomMissIndex = Math.floor( Math.random() * this.missText.length);
+    this.randomAttackIndex = Math.floor( Math.random() * this.attackText.length);
+    this.randomDeathIndex = Math.floor( Math.random() * this.deathText.length);
+    }
+    wait(){
 
   }
 }
-  
-let player = new Player();
-let enemy = new Enemy("bug");
-player.setBattleText(enemy);
-enemy.setBattleText(player);
