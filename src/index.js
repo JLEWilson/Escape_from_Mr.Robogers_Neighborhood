668efa4c319d3d0ed1c62c16fffcc8a619ff9942
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-29 15:31:59 -0900

    fix merge conflicts
    Co-Authored-by: Zach Wilson <zwilson22495@gmail.com>
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-Authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import './css/styles.css';
 import $ from "jquery";
 import { Player, Enemy } from './js/entity';
 
+//make an instance of each object
+
+let waltDisney = new Animator();
+let gameMaps = new LevelMap();
+let player1 = new Player(waltDisney, gameMaps);
 let player;
 const enemy = new Enemy("bug");
 
+export function createRoom() {
+  $("#map-background").html("");
+  for(let i = 0; i< 10; i++){
+    for(let j = 0; j<10;j++){
+      $("#map-background").append(`<div class="${gameMaps.Rooms[player1.z][j][i].texture} tile"></div>`);
+    }
+  }
+}
+
+createRoom();
+
+//buttons
 $('#character-create-form').submit((event) => {
   event.preventDefault();
   const name = $('#charName').val();
@@ -47,3 +64,41 @@ $('#attack').click(() => {
     $('.story-area').append("<p>" + player.missText[player.randomMissText] + "</p>");
   }
 })
+
+
+//make a controller
+
+var keysDown = {};
+
+document.addEventListener("keydown",(e)=>{keysDown[e.keyCode] = true;}, false);
+document.addEventListener('keyup',(e)=>{delete keysDown[e.keyCode];}, false);
+
+function takeInput(){
+  if (38 in keysDown ) {
+    player1.move('N');
+  }
+  if(40 in keysDown){
+    player1.move('S');
+  }
+  if(39 in keysDown){
+    player1.move('E');
+  }
+  if(37 in keysDown){
+    player1.move('W');
+  }
+}
+
+//misc
+let background = document.getElementById('map-background');
+background.style.width = waltDisney.screen.width;
+background.style.height = waltDisney.screen.height;
+
+//make a loop
+
+function loopA(){
+  takeInput();
+  waltDisney.clear();
+  player1.drawSelf();
+  setTimeout(()=>{requestAnimationFrame(loopA);} , 1000/12);
+}
+loopA();
